7080e1c58b4094376c6c1a73a03779c17299feec
vishnu2k5 <mvishnuvardhan156@gmial.com>
2025-09-17 16:52:13 +0530

    c1

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+      <ul className='navbar'>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
