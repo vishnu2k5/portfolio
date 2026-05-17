485fca77c80c4fb25e0ebf184c269ebde07d028b
vishnu <143354384+vishnu2k5@users.noreply.github.com>
2026-05-17 12:43:09 +0530

    Update ongoing project ID from 6 to 1

--- frontend/projectStore/ongoing.js
+++ frontend/projectStore/ongoing.js
@@ -3,7 +3,7 @@ import { create } from 'zustand';
 export const useOngoingStore = create((set) => ({
     ongoing: [
         {
-      id: 6,
+      id: 1,
       name: 'AI EMS_BOM ',
       github: 'https://github.com/vishnu2k5/aiems.git',
       image: 'https://res.cloudinary.com/dtujitvpy/image/upload/v1779001634/ems_daeack.png',
