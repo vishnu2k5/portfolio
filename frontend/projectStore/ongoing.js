9ed854042f44ea33d75c1900e1bd3d1ec95d80db
vishnu <143354384+vishnu2k5@users.noreply.github.com>
2026-05-17 12:42:56 +0530

    Add AI EMS_BOM project to ongoing store
    Added a new ongoing project entry for AI EMS_BOM with details.

--- frontend/projectStore/ongoing.js
+++ frontend/projectStore/ongoing.js
@@ -2,5 +2,15 @@ import { create } from 'zustand';
 
 export const useOngoingStore = create((set) => ({
     ongoing: [
+        {
+      id: 6,
+      name: 'AI EMS_BOM ',
+      github: 'https://github.com/vishnu2k5/aiems.git',
+      image: 'https://res.cloudinary.com/dtujitvpy/image/upload/v1779001634/ems_daeack.png',
+      description:
+         'Developed a full-stack automated quotation platform for Electronics Manufacturing Services (EMS) using React, Python (FastAPI), and Docker. Engineered an intelligent backend to parse Bills of Materials (BOMs) and automate complex pricing algorithms for PCB fabrication, assembly, and customs duties. Built interactive analytics dashboards for real-time margin analysis and deployed the containerized application on AWS using Nginx.',
+      live:
+        '',
+    },
     ],
 }));
